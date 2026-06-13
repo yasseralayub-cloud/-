a5f0086f9785f620b82c1a3f0d7c9a498c99f21f
yasseralayub-cloud <yasseralayub@gmail.com>
2026-06-13 08:27:49 +0300

    Update PublicMenu.tsx

--- src/pages/PublicMenu.tsx
+++ src/pages/PublicMenu.tsx
@@ -145,7 +145,7 @@ export default function PublicMenu() {
               <div style={{ textAlign: 'center', marginTop: '20px', marginBottom: '20px' }}>
   <div style={{ textAlign: 'center', marginTop: '25px', marginBottom: '25px' }}>
   <a
-    href="https://luxcod-1024305356158.europe-west2.run.app"
+    href="https://luxcod.online/"
     target="_blank"
     rel="noopener noreferrer"
     style={{
